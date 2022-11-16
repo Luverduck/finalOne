5a6be3cd24ad36068e392e474144a4826ecb205f
Luverduck <hyekko92@gmail.com>
2022-11-16 23:07:06 +0900

    Test 생성

---
+++ ahzit/src/test/java/com/kh/ahzit/Test_public1992.java
@@ -0,0 +1,16 @@
+package com.kh.ahzit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class Test_public1992 {
+
+	@Test
+	public void test() {
+		
+	}
+}
--- ahzit/src/test/java/com/kh/ahzit/Test_sjtees.java
+++ ahzit/src/test/java/com/kh/ahzit/Test_sjtees.java
@@ -3,11 +3,14 @@ package com.kh.ahzit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
-class AhzitApplicationTests {
+public class Test_sjtees {
 
 	@Test
-	void contextLoads() {
+	public void test() {
+		
 	}
-
 }
