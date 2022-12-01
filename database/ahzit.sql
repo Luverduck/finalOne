27358824080e49107980ff52959f7df78f98186b
sjtees <109942106+sjtees@users.noreply.github.com>
2022-12-01 16:35:30 +0900

    Update ahzit.sql

--- database/ahzit.sql
+++ database/ahzit.sql
@@ -7,7 +7,8 @@ ahzit_sort varchar2(9) not null check(ahzit_sort in('취미','스터디','일상
 ahzit_name varchar2(60) not null,
 ahzit_inforo varchar(300) not null,
 ahzit_head number default 1 not null check(ahzit_head >= 1),
-ahzit_region varchar2(12) not null,
+ahzit_region_high varchar2(15) not null,
+ahzit_region_low varchar2(9) not null,
 ahzit_like number default 0 not null check(ahzit_like >= 0),
 ahzit_score number default 0 not null check(ahzit_score >= 0),
 ahzit_ispublic char(1) not null check(ahzit_ispublic in('Y','N'))
