c080be98491a46fe83908542ae4f362a3ae7365e
public1992 <livethereality22@gmail.com>
2022-12-02 15:42:23 +0900

    소모임 삭제 메소드 void가 아니라 boolean으로

--- ahzit/src/test/java/com/kh/ahzit/Test_public1992.java
+++ ahzit/src/test/java/com/kh/ahzit/Test_public1992.java
@@ -20,9 +20,25 @@ public class Test_public1992 {
 	@Autowired
 	private AhzitDao ahzitDao;
 
+	//삭제
+	//ahzitDao.delete(AhzitDto.builder().ahzitNo(4).ahzitLeader("tester111").build());
+	
+	//아지트생성
+//	int test=ahzitDao.sequence();
+//	sqlSession.insert("ahzit.insert", AhzitDto.builder()
+//												.ahzitNo(test)
+//												.ahzitLeader("tester222")
+//												.ahzitSort("스터디")
+//												.ahzitName("더이상미룰수없다우리들의취업")
+//												.ahzitInfo("부끄러움이 많은 인생이었습니다")
+//												.ahzitRegionHigh("황해도")
+//												.ahzitRegionLow("탄광촌")
+//												.ahzitIsPublic("Y")
+//												.build());
+	
 	@Test
 	public void test() {
-		
+		System.out.println(ahzitDao.delete(AhzitDto.builder().ahzitNo(4).ahzitLeader("tester111").build()));
 		
 	}
 
