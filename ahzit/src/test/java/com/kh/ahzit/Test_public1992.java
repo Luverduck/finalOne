b9d700f6ffe5ec855d570eb4b19d7116c8ab026d
public1992 <livethereality22@gmail.com>
2022-12-02 14:33:56 +0900

    아지트 삭제 메소드 만드는중

--- ahzit/src/test/java/com/kh/ahzit/Test_public1992.java
+++ ahzit/src/test/java/com/kh/ahzit/Test_public1992.java
@@ -22,18 +22,8 @@ public class Test_public1992 {
 
 	@Test
 	public void test() {
-		int test=ahzitDao.sequence();
-		AhzitDto ahzitDto=AhzitDto.builder()
-												.ahzitNo(test)
-												.ahzitLeader("tester111")
-												.ahzitSort("취미")
-												.ahzitName("테스트아지트생성메소드")
-												.ahzitInfo("인원제한칼럼추가")
-												.ahzitRegionHigh("서울시")
-												.ahzitRegionLow("낙원구")
-												.ahzitIsPublic("Y")
-												.build();
-		sqlSession.insert("ahzit.ahzitInsert",ahzitDto);
+		
+		
 	}
 
 }
