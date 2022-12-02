fa141887d10d88c74be865fab29a4dfef3e868da
Luverduck <109222367+Luverduck@users.noreply.github.com>
2022-12-02 11:26:47 +0900

    Update ahzit.sql

--- database/ahzit.sql
+++ database/ahzit.sql
@@ -12,7 +12,7 @@ ahzit_region_low varchar2(9) not null,
 ahzit_like number default 0 not null check(ahzit_like >= 0),
 ahzit_score number default 0 not null check(ahzit_score >= 0),
 ahzit_state char(1) default 'N' check(ahzit_state in ('Y', 'N')) not null,
-ahzit_ispublic char(1) not null check(ahzit_ispublic in('Y','N'))
+ahzit_ispublic char(1) not null check(ahzit_ispublic in('Y','N')),
 ahzit_grade char(6) default '씨앗' check(ahzit_grade in('씨앗','새싹', '잎새', '가지', '열매', '나무')) not null
 );
 
