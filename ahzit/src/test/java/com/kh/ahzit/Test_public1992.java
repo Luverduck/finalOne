926da778f518fef5365de34072e7aee527b196b2
public1992 <livethereality22@gmail.com>
2022-12-02 11:50:58 +0900

    ahzit-mapper파일 생성, 시퀀스 발행 메소드, application.properties에 강의실 내부 오라클 접속 설정

--- ahzit/src/test/java/com/kh/ahzit/Test_public1992.java
+++ ahzit/src/test/java/com/kh/ahzit/Test_public1992.java
@@ -1,17 +1,24 @@
 package com.kh.ahzit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.ahzit.repository.AhzitDao;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
 public class Test_public1992 {
+	
+	@Autowired
+	private AhzitDao ahzitDao;
 
 	@Test
 	public void test() {
-		String test="병합 테스트 12011705";
-		String test2="configuration fetch 설정 main 브랜치, 내 브랜치만 받도록 변경하여 다시 테스트";
+		int test=ahzitDao.sequence();
+		System.out.println(test);
 	}
+
 }
