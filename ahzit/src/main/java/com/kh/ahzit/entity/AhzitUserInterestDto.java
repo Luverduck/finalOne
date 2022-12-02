4bd223b89cd61ac01d3e8005ac4345c3d727f18e
public1992 <livethereality22@gmail.com>
2022-12-02 12:04:47 +0900

    유저 관심사 테이블, 인증키 테이블 dto 생성

---
+++ ahzit/src/main/java/com/kh/ahzit/entity/AhzitUserInterestDto.java
@@ -0,0 +1,13 @@
+package com.kh.ahzit.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class AhzitUserInterestDto {
+	private int userInterestNo;
+	private String userInterestId;
+	private String userInterestSort;
+}
