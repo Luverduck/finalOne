df630a7c0984664f426f68f8c38b3f3007c16c4f
public1992 <livethereality22@gmail.com>
2022-12-02 13:56:05 +0900

    ahzit-mapper에 아지트 생성 메소드. 테스트 확인

--- ahzit/src/test/java/com/kh/ahzit/Test_public1992.java
+++ ahzit/src/test/java/com/kh/ahzit/Test_public1992.java
@@ -1,9 +1,11 @@
 package com.kh.ahzit;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.kh.ahzit.entity.AhzitDto;
 import com.kh.ahzit.repository.AhzitDao;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,12 +15,25 @@ import lombok.extern.slf4j.Slf4j;
 public class Test_public1992 {
 	
 	@Autowired
+	private SqlSession sqlSession;
+	
+	@Autowired
 	private AhzitDao ahzitDao;
 
 	@Test
 	public void test() {
 		int test=ahzitDao.sequence();
-		System.out.println(test);
+		AhzitDto ahzitDto=AhzitDto.builder()
+												.ahzitNo(test)
+												.ahzitLeader("tester111")
+												.ahzitSort("취미")
+												.ahzitName("테스트아지트생성메소드")
+												.ahzitInfo("인포메이션이면아지트소개문인가")
+												.ahzitRegionHigh("경기도")
+												.ahzitRegionLow("성남시")
+												.ahzitIsPublic("Y")
+												.build();
+		sqlSession.insert("ahzit.ahzitInsert",ahzitDto);
 	}
 
 }
