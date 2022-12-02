d79cee32fb2f50963337f99b3f7e6d0acae26f8c
Luverduck <109222367+Luverduck@users.noreply.github.com>
2022-12-02 16:00:44 +0900

    Update ahzit_board.sql

--- database/ahzit_board.sql
+++ database/ahzit_board.sql
@@ -2,7 +2,7 @@
 -- 테이블 생성
 create table ahzit_board (
 board_no number primary key,
-board_writer references ahzit_member(member_nick) on delete set null,
+board_writer_no references ahzit_member(member_no) on delete set null,
 board_content varchar2(4000) not null,
 board_read number default 0 check(board_read >= 0) not null,
 board_like number default 0 check(board_like >= 0) not null,
@@ -22,6 +22,3 @@ drop sequence ahzit_board_seq;
 
 -- 게시글 번호 반환
 select ahzit_board_seq.nextval from dual;
-
--- 게시글 등록
-insert into ahzit_board(board_no, board_writer, board_content) values (ahzit_board_seq.nextval, '테스터222', '테스트게시글');
