ca0a47469ff58121409bb31a799c789532bc0afe
soo231 <a995115@gmail.com>
2022-12-03 14:29:57 +0900

    아지트 수정 테스트

--- ahzit/src/test/java/com/kh/ahzit/AhzitUpdateTest.java
+++ ahzit/src/test/java/com/kh/ahzit/AhzitUpdateTest.java
@@ -16,11 +16,11 @@ public class AhzitUpdateTest {
 	@Test
 	public void test() {//수정은 소모임명, 소개, 최대멤버수, 공개여부, 프로필사진
 			int count = sqlSession.update("ahzit.ahzitUpdate", AhzitDto.builder()
-					.ahzitName("소모임이름수정") //소모임이름
-					.ahzitInfo("소모임소개수정") //소모임소개
-					.ahzitHeadMax(100) //소모임최대인원
-					.ahzitIsPublic("N") //소모임 공개여부 
-					.ahzitNo(20)
+					.ahzitName("테스트용아지트4") //소모임이름
+					.ahzitInfo("테스트용아지트소개4") //소모임소개
+					.ahzitHeadMax(10) //소모임최대인원
+					.ahzitIsPublic("Y") //소모임 공개여부 
+					.ahzitNo(23)
 					.build());
 			
 			System.out.println("count = " + count);
