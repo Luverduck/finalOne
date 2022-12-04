8d6ce2e4c34cdba1941df78db1353fe544cf94d4
sjtees <109942106+sjtees@users.noreply.github.com>
2022-12-04 18:18:32 +0900

    Update certification.sql
    인증키 테이블 sql 수정 - 외래키 설정 삭제 / certification_no 및 시퀀스 생성

--- database/certification.sql
+++ database/certification.sql
@@ -1,10 +1,13 @@
 -- 인증키(certification) 테이블
 -- 테이블 생성
 create table certification(
-certification_id references ahzit_user(user_id) on delete cascade not null,
+certification_no number primary key,
+certification_id varchar(20) not null,
 certification_key varchar(20) not null,
 certification_issuedate date default sysdate not null
 );
+
+create SEQUENCE certification_seq;
 
 -- 테이블 삭제
 drop table certification;
