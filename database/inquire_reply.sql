09cf195624642960e90c2487c7798977952d555d
Luverduck <109222367+Luverduck@users.noreply.github.com>
2022-12-05 23:40:36 +0900

    문의 게시글 댓글 상태(inquire_reply_state) 컬럼 추가
    문의 게시글 댓글 상태(inquire_reply_state) 컬럼 추가

--- database/inquire_reply.sql
+++ database/inquire_reply.sql
@@ -6,7 +6,8 @@ inquire_origin_no references inquire(inquire_no) on delete cascade not null,
 inquire_reply_writer references ahzit_user(user_id) on delete cascade not null,
 inquire_reply_content varchar(900) not null,
 inquire_reply_writedate date default sysdate not null,
-inquire_reply_updatedate date
+inquire_reply_updatedate date,
+inquire_reply_state char(1) default 'N' check(inquire_reply_state in('Y','N')) not null
 );
 
 -- 테이블 삭제
