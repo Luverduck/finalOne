83f37176dba794808b26494eea3c64d6029d429b
Luverduck <hyekko92@gmail.com>
2022-12-05 09:50:22 +0900

    작업 내용
    freeboardlike-mapper.xml
    - 자유 게시글 좋아요 등록 / 삭제 / 좋아요 여부 확인을 위한 갯수 반환 / 좋아요 갯수 갱신 구현
    
    FreeboardLikeDao, FreeboardLikeDaoImpl
    - 자유 게시글 좋아요 등록 / 삭제 / 좋아요 여부 확인을 위한 갯수 반환 / 좋아요 갯수 갱신 구현
    
    FreeboardController
    - 좋아요 기능 구현

--- ahzit/src/main/java/com/kh/ahzit/repository/FreeboardLikeDaoImpl.java
+++ ahzit/src/main/java/com/kh/ahzit/repository/FreeboardLikeDaoImpl.java
@@ -18,13 +18,13 @@ public class FreeboardLikeDaoImpl implements FreeboardLikeDao {
 
 	// 추상 메소드 오버라이딩 - 자유 게시글 좋아요 등록
 	@Override
-	public void freeboardLikeInsert(FreeboardLikeDto freeboardLikeDto) {
+	public void insertFreeboardLike(FreeboardLikeDto freeboardLikeDto) {
 		sqlSession.insert("freeboard_like.insert", freeboardLikeDto);
 	}
 
 	// 추상 메소드 오버라이딩 - 자유 게시글 좋아요 여부를 위한 갯수 조회
 	@Override
-	public boolean freeboardLikeCheck(int freeboardLikeNo, String freeboardLikeId) {
+	public boolean checkFreeboardLike(int freeboardLikeNo, String freeboardLikeId) {
 		// 변수 바인딩을 위한 Map 생성
 		Map<String, String> param = new HashMap<>();
 		// 생성한 Map의 type과 keyword를 freeboardListSeachVO의 type과 keyword로 설정
@@ -36,7 +36,13 @@ public class FreeboardLikeDaoImpl implements FreeboardLikeDao {
 
 	// 추상 메소드 오버라이딩 - 자유 게시글 좋아요 삭제
 	@Override
-	public boolean freeboardLikeDelete(FreeboardLikeDto freeboardLikeDto) {
+	public boolean deleteFreeboardLike(FreeboardLikeDto freeboardLikeDto) {
 		return sqlSession.delete("freeboard_like.delete", freeboardLikeDto) > 0;
+	}
+
+	// 추상 메소드 오버라이딩 - 자유 게시글 좋아요 총 갯수 갱신
+	@Override
+	public boolean updateFreeboardLikeCount(int freeboardNo) {
+		return sqlSession.update("freeboard_like.updateCount", freeboardNo) > 0;
 	}
 }
