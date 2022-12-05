5ab8d06a8e16cde29233756704c308dc54f63388
seungrii <choevictory@gmail.com>
2022-12-05 12:02:25 +0900

    공지게시판 crud mybatis 사용하여 구현

--- ahzit/src/test/java/com/kh/ahzit/Test_seungrii.java
+++ ahzit/src/test/java/com/kh/ahzit/Test_seungrii.java
@@ -1,50 +1,29 @@
 package com.kh.ahzit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.sql.Date;
 
-import java.util.List;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.kh.ahzit.entity.NoticeDto;
-import com.kh.ahzit.repository.NoticeDao;
-
-import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
 public class Test_seungrii {
 
 	@Autowired
-	private NoticeDao noticeDao;
-	
-	@BeforeEach
-	public void before() {
-		for(int i=1; i<=5; i++) {
-			noticeDao.insert(NoticeDto.builder()
-						.noticeTitle("테스트"+i)
-						.noticeContent("테스트"+i)
-						.noticeWriter("admin123")				
-					.build());
-		}
-	}
+	private SqlSession sqlSession;
 	
 	@Test
 	public void test() {
-		//조회
-		List<NoticeDto> list = noticeDao.selectList();
-		
-		for(NoticeDto noticeDto : list) {
-			System.out.println(noticeDto);
-		}
-		assertEquals(list.size(), 5);
+		//assertNotNull(sqlSession);
+		NoticeDto noticeDto = NoticeDto.builder()
+						.noticeNo(62)
+						.noticeTitle("바꾼제목")
+						.noticeContent("바꾼내용")
+						.noticeWriter("admin123")
+					.build();
+		sqlSession.insert("notice.insert", noticeDto);
 	}
-	
-//	 @AfterEach
-//	 public void after() {
-//	 	noticeDao.clear();
-//	 }
 }
