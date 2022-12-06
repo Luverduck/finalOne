4bad79c94a2bc49ea5c120f9048de58f3a1cee49
Luverduck <109222367+Luverduck@users.noreply.github.com>
2022-12-06 14:21:35 +0900

    게시글 좋아요 등록, 좋아요 여부를 위한 갯수, 좋아요 총 갯수, 좋아요 갱신 추가
    게시글 좋아요 등록, 좋아요 여부를 위한 갯수, 좋아요 총 갯수, 좋아요 갱신 추가

--- database/freeboard_like.sql
+++ database/freeboard_like.sql
@@ -4,3 +4,19 @@ create table freeboard_like (
 freeboard_like_no references freeboard(freeboard_no) on delete cascade,
 freeboard_like_id references ahzit_user(user_id) on delete set null
 );
+
+-- 자유 게시글 좋아요 등록
+-- insert into freeboard_like(freeboard_like_no, freeboard_like_id) values(#{freeboardLikeNo}, #{freeboardLikeId})
+
+-- 자유 게시글 좋아요 여부를 위한 갯수 조회
+-- select count(*) from freeboard_like where freeboard_like_no = #{freeboardLikeNo} and freeboard_like_id = #{freeboardLikeId}
+
+-- 자유 게시글 좋아요 삭제
+-- delete freeboard_like where freeboard_like_no = #{freeboardLikeNo} and freeboard_like_id = #{freeboardLikeId}
+
+-- 자유 게시글 좋아요 총 갯수
+select count(*) from freeboard_like where freeboard_like_no = 187;
+-- select count(*) from freeboard_like where freeboard_like_no = #{freeboardLikeNo}
+
+-- 자유 게시글 좋아요 갱신
+update freeboard set freeboard_like = #{freeboardLike} where freeboard_no = #{freeboardNo}
