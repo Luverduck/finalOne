fb154ffbbb67ee24cdab30a29fd528147b145fdb
soo231 <a995115@gmail.com>
2022-12-07 18:16:59 +0900

    board.jsp - 소모임 정보 불러오는 구문 추가 ahzitIncontroller - 소모임 정보 불러오는 구문 추가 ahzitVO - 소모임 정보 불러오기 위한 vo 생성 ahzitMemberListVO - 구문 오타 수정 ahzitController - 소모임가입자 증가를 위한 구문 추가 ahzitDao, ahzitDaoImpl - 아지트 가입하기메소트, 참가자인원수증가 구문추가 ahzitLeader-mapper - 소모임 생성하면 리더가 자동가입하는 매퍼 생성 ahzitmember-mapper - 소모임 가입을 위한 구문 추가

--- ahzit/src/main/java/com/kh/ahzit/repository/AhzitDao.java
+++ ahzit/src/main/java/com/kh/ahzit/repository/AhzitDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.kh.ahzit.entity.AhzitAttachmentDto;
 import com.kh.ahzit.entity.AhzitDto;
+import com.kh.ahzit.entity.AhzitMemberDto;
 
 public interface AhzitDao {
 	int sequence();//ahzit_no 시퀀스 번호 발행
@@ -12,6 +13,12 @@ public interface AhzitDao {
 	void ahzitAttachment(AhzitAttachmentDto ahzitAttachmentDto); //ahzit_attachment 테이블에 첨부파일 정보 연결
   
 	void insert(AhzitDto ahzitDto);//아지트 생성
+	
+	void addMember(AhzitMemberDto ahzitMemberDto); //아지트생성에 개설자 자동 추가
+	void insertMember(AhzitMemberDto ahzitMemberDto);//아지트가입하기 메소드
+	
+	public boolean updateAhzitPerson(int ahzitNo); //아지트 참가자 인원수 증가
+	
 	List<AhzitDto> selectList();//아지트 목록
 	AhzitDto selectOne(int ahzitNo);//아지트 단일조회
 	public int ahzitEdit(AhzitDto ahzitDto);//아지트 수정
